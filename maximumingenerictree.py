aa8613d7b9b2ec3a6c8fe74e45f79cfa56fd39a0
nishu959 <60173728+nishu959@users.noreply.github.com>
2021-06-26 00:35:22 +0530

    Add files via upload

---
+++ maximumingenerictree.py
@@ -0,0 +1,42 @@
+import sys
+class Tree():
+  def __init__(self, data):
+    self.data = data
+    self.child = []
+   
+m = int(input())
+ip = list(map(int, input().split()))
+l = []
+
+
+for i in ip:
+  if(i==-1):
+    l.pop()
+  else:
+    node = Tree(i)
+    if(len(l)==0):
+      root = node
+    else:
+      l[-1].child.append(node)
+    l.append(node)
+    
+
+def maxintree(root):
+  m = -1*sys.maxsize 
+  for i in root.child:
+    p = maxintree(i)
+    m = max(p, m)
+    
+  if(root.data>m):
+    m = root.data
+  return m
+  
+  
+  
+print(maxintree(root))
+ 
+ 
+    
+  
+
+
